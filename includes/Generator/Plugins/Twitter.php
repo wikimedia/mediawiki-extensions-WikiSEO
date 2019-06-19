5c2c47975a1fe6ce425d0bcbe98b5a10ce59cd85
Hannes <hannes@octofox.de>
2019-06-19 17:23:45 +0200

    :sparkles: Allow to set twitter site handle via settings

--- includes/Generator/Plugins/Twitter.php
+++ includes/Generator/Plugins/Twitter.php
@@ -10,19 +10,7 @@ use Html;
  * @package Octfx\WikiSEO\Generator\Plugins
  */
 class Twitter extends OpenGraph {
-	protected static $tags = [
-		'type',
-		'image',
-		'image_width',
-		'image_height',
-		'description',
-		'keywords',
-		'locale',
-		'site_name',
-		'published_time',
-		'modified_time',
-		'twitter_site',
-	];
+	protected static $tags = [ 'type', 'image', 'image_width', 'image_height', 'description', 'keywords', 'locale', 'site_name', 'published_time', 'modified_time', 'twitter_site', ];
 
 	/**
 	 * Page title property name
@@ -48,11 +36,24 @@ class Twitter extends OpenGraph {
 	 * @return void
 	 */
 	public function addMetadata() {
+		$this->addTwitterSiteHandleTag();
+
 		parent::addMetadata();
 
-		$this->outputPage->addHeadItem( 'twitter:card', Html::element( 'meta', [
-			self::$htmlElementPropertyKey => 'twitter:card',
-			self::$htmlElementContentKey  => 'summary'
-		] ) );
+		$this->outputPage->addHeadItem( 'twitter:card', Html::element( 'meta', [ self::$htmlElementPropertyKey => 'twitter:card', self::$htmlElementContentKey => 'summary' ] ) );
+	}
+
+	/**
+	 * Add the global twitter site handle from $wgTwitterSiteHandle to the meta tags
+	 * If $wgTwitterSiteHandle is not null setting the handle via tag or hook is ignored
+	 */
+	private function addTwitterSiteHandleTag() {
+		global $wgTwitterSiteHandle;
+
+		if ( $wgTwitterSiteHandle !== null ) {
+			unset( self::$tags['twitter_site'], self::$conversions['twitter_site'], $this->metadata['twitter_site'] );
+
+			$this->outputPage->addHeadItem( 'twitter:site', Html::element( 'meta', [ self::$htmlElementPropertyKey => 'twitter:site', self::$htmlElementContentKey => $wgTwitterSiteHandle ] ) );
+		}
 	}
 }
